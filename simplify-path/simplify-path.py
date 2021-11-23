add05fec95ec6b07ed0b0e324388345e1bc6f27d
TracyWang0110 <chunxue.wang0110@gmail.com>
2021-11-23 15:35:00 -0600

    Time: 88 ms (5.28%), Space: 14.4 MB (15.09%) - LeetHub

--- simplify-path/simplify-path.py
+++ simplify-path/simplify-path.py
@@ -4,9 +4,11 @@ class Solution:
         arr = path.split("/")
         print(arr)
         stack=[]
+        #会遇到什么呢，会遇到.,空串，，..
         for ch in arr:
+            #返回上级目录
             if ch == "..":
-                if stack: #判定是不是空
+                if stack: #判定是不是空，在写Pop之前就一定要判定
                     stack.pop()
                     print (arr)
             elif ch =='.'or ch =='': 
@@ -15,9 +17,14 @@ class Solution:
                 stack.append(ch)
         print (stack)
         
+        '''
         strs=''
+        #如果栈是空的，返回跟目录
         if not stack:
             return '/'
         for s in stack:
             strs= strs+ '/'+s
         return strs
+        '''
+        return '/'+'/'.join(stack)
+        
